57ff5a608721cf2149ee0f45fb3cbd0119bc4172
Jami Gibbs <jami0821@gmail.com>
2021-01-08 17:51:49 -0600

    Initial WIP

---
+++ urls.js
@@ -0,0 +1,11 @@
+const baseUrl = 'https://developer.salesforce.com/docs/component-library/bundle';
+
+const urls = [
+  {
+    name: 'lightning-accordion',
+    docs: `${baseUrl}/lightning-accordion/documentation`,
+    specs: `${baseUrl}/lightning-accordion/specification`
+  }
+];
+
+module.exports = urls;
